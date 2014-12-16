ec69a47fad08c47581befef2c9027c259899c03c
Tim Ribaric <tim.ribaric@gmail.com>
2014-12-16 09:51:41 -0500

    Bit better
    Splits out into different files based on code

--- prep_book_list.py
+++ prep_book_list.py
@@ -1,24 +1,36 @@
 #
 # Preps New Book list for Yahoo Pipes
-#  Trivially Simple
+#  Less trivially Simple
 #
-# opens csv file, dumps out xml
+# opens csv file, dumps out CSV
 import csv
+import os
 
-try:
-    f = csv.reader(open('books_in.csv','r'))
-except:
-    print "Input file not opened"
-    exit;
- 
-try:
-    o = open('new_books.csv','w')
-except:
-    print "Output file not opened"
 
-f.next()
-o.write('URL,TITLE\n')
-for r in f:
-    o.write("http://catalogue.library.brocku.ca/search/a?searchtype=c&searcharg="+r[1].replace(' ','+')+','+r[2]+'\n')
+codes = {'h54': [] , 'm66' : []}
 
-print "done"
+
+if __name__ == "__main__":
+  print "Resolving",
+  for f in os.listdir(os.getcwd()):
+    if f[-3:] == "csv" and f != "new_books.csv":
+      #infile = open(f,"r")
+        infile = csv.reader(open(f,'r'), delimiter=',',quotechar='"')
+        break
+  if not infile:
+    print " input file not found"
+    exit
+  print ""
+
+for line in infile:
+    if line[0].strip() in codes.keys():
+        codes[line[0].strip()].append("http://catalogue.library.brocku.ca/search/a?searchtype=c&searcharg="+line[1].replace(' ','+')+','+'"'+line[2]+'"'+'\n')
+
+
+for l in codes:
+    o = open( l+'.csv', 'w')
+    o.write('URL,TITLE\n')
+    for items in codes[l]:
+        o.write(items)
+    o.close()
+
